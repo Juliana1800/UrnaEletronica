1a26251309d09b944f6b55ab06ba3f68ea20e3cf
juliana <juliana@senai>
2023-10-10 11:49:41 -0300

    ainda nao entendi js

---
+++ urnaEletronica.js
@@ -0,0 +1,23 @@
+function urnaEletronica() {
+
+    let opcao;
+    let contador = 0;
+
+    do {
+
+        console.log ('Repetição', contadorTotalDeVotos=0);
+
+        opcao = parseInt(prompt("Digite a opção: "));
+
+        contadorTotalDeVotos ++;
+        
+
+    } while (opcao !==0);
+
+    contadorTotalDeVotos--;
+    
+    console.log ("Contagem:", contador);
+
+
+}
+//urnaEletronica(); //aqui chamo a função 
