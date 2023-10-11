7a8e82d03304e5490a52e16b3b5612ba17a2ed1a
Juliana <Juliana@gmail.com>
2023-10-11 11:52:30 -0300

    fazendo condicional de candidatos

--- urnaEletronica.js
+++ urnaEletronica.js
@@ -1,23 +1,87 @@
 function urnaEletronica() {
 
-    let opcao;
-    let contador = 0;
+let
+    opcao;
+    contadorTotal=0;
+    candidato1=0;
+    candidato2=0;
+    candidato3=0;
+    branco=0;
+    nulo=0;
+    nomeCandidato1 = 'Maria';
+    nomeCandidato2 = 'Jenovaldo';
+    nomeCandidato3 = 'Marionete';    
 
+    console.log ("Inicio da votação");
+   
     do {
+        opcao = parseInt(prompt("| 1 | Candidato 1" + '\n' +
+                                "| 2 | Candidato 2" + '\n' +
+                                "| 3 | Candidato 3" + '\n' +
+                                "| 4 | branco " + '\n' +
+                                "| 5 | nulo" + '\n' + '\n' +
+                               "Digite a opção: "));
+        contadorTotal++;
 
-        console.log ('Repetição', contadorTotalDeVotos=0);
+     
+        if (opcao==1) {
+            console.log ("Você votou no candidato 1");
+            alert("O nome do candidato é: " + nomeCandidato1 );
+            candidato1++;
 
-        opcao = parseInt(prompt("Digite a opção: "));
+        }else if (opcao==2) {
+            console.log ("Você votou no candidato 2");
+            alert("O nome do candidato é: " + nomeCandidato2 );
+            candidato2++;
 
-        contadorTotalDeVotos ++;
-        
+        }else if (opcao==3) {
+            console.log ("Vocô votou no candidato 3");
+            alert("O nome do candidato é: " + nomeCandidato2 );
+            candidato3++;
+
+        }else if (opcao==4) {
+            console.log ("Branco");
+            branco++;
+
+        }else if (opcao==5) {
+            console.log ("Nulo");
+            nulo++;
+
+        }else if (opcao==0) {
+            alert ("Votação encerrada");
+            contadorTotal--;
+
+        }else{
+            console.log ("Voto Inválido");
+            contadorTotal--;
+        }
 
     } while (opcao !==0);
 
-    contadorTotalDeVotos--;
+        
+    // contadorTotalDeVotos--;
     
-    console.log ("Contagem:", contador);
+    console.log ("Contagem total de votos: " + contadorTotal);
+    console.log ("Candidato 1: " + candidato1/contadorTotal*100 + " % " );
+    console.log ("Candidato 2: " + candidato2/contadorTotal*100 + " % " );
+    console.log ("Candidato 3: " + candidato3/contadorTotal*100 + " % ");
+    console.log ("Votos em branco: " + branco/contadorTotal*100 + " % ");
+    console.log ("Votos nulos: " + nulo/contadorTotal*100 + " % ");
+    if (candidato1 > candidato2 && candidato1> candidato3) {
+        console.log ("Candidato 1 ganhador: " + nomeCandidato1);
 
+        }else if (candidato2 > candidato1 && candidato2> candidato3) {
+        console.log ("Candidato 2 ganhador: " + nomeCandidato2);
+
+        }else if (candidato3 > candidato1 && candidato3> candidato2) {
+        console.log ("Candidato 3 ganhador: " + nomeCandidato1);
+
+        }else if (candidato1==candidato2 || candidato2==candidato3 || candidato3==candidato1) {
+            console.log ("Empate");
+        }
+    
+    porcentagem=contadorTotal/100;
+    // console.log("Resultado Final: " + porcentagem);
 
 }
 //urnaEletronica(); //aqui chamo a função 
