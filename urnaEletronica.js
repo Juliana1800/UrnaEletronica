1696f5a1b7525e29c5e1f77def2d8b81a699e9b6
Juliana <jrtokura@gmail.com>
2023-10-27 10:59:08 -0300

    desafio8

--- urnaEletronica.js
+++ urnaEletronica.js
@@ -10,9 +10,13 @@ function urnaEletronica() {
         nulo = 0;
         encerrar= "",
         pin=0;
+        dataAtual = new Date();
+        
+
     
         do{
-            console.log ("**Inicío da votação**");
+            // Desafio#8
+           
             pin=parseInt(prompt("Senha mesário: "));  
             nomeCandidato1= prompt("Digite o nome do candidato 1: ");
             nomeCandidato2= prompt("Digite o nome do candidato 2: ");
@@ -83,15 +87,20 @@ function urnaEletronica() {
 
     } while (encerrar !== 'S')
 
-    console.clear();
-    console.log('** BOLETIM DE URNA **');
+    // console.clear();
+    dataFinal = new Date();
+    console.log ("**Boletim de votação**");
+    console.log ("**Inicío da votação**" + " " + dataAtual);;
+    console.log ('**Fim da Votação**' + " " + dataFinal);
+    
     console.log ("Total de votos: " + contadorTotal);
-    console.log("Total de votos candidato 1: " + candidato1 + " votos (" + (candidato1 / contadorTotal * 100).toFixed(2) + " % )");
-    console.log("Total de votos candidato 2: " + candidato2 + " votos (" + (candidato2 / contadorTotal * 100).toFixed(2) + " % )");
-    console.log("Total de votos candidato 3: " + candidato3 + " votos (" + (candidato3 / contadorTotal * 100).toFixed(2) + " % )");
+    console.log("Total de votos candidato 1: " + nomeCandidato1 + " votos (" + (candidato1 / contadorTotal * 100).toFixed(2) + " % )");
+    console.log("Total de votos candidato 2: " + nomeCandidato2 + " votos (" + (candidato2 / contadorTotal * 100).toFixed(2) + " % )");
+    console.log("Total de votos candidato 3: " + nomeCandidato3 + " votos (" + (candidato3 / contadorTotal * 100).toFixed(2) + " % )");
     console.log("Total votos em branco: " + branco + " votos (" + (branco / contadorTotal * 100).toFixed(2) + " % )");
     console.log("Total votos nulos: " + nulo + " votos (" + (nulo / contadorTotal * 100).toFixed(2) + " % )");
 
+    
     if (candidato1 > candidato2 && candidato1 > candidato3) {
         candidato1=candidato1+branco;
         console.log("Candidato 1 ganhador: " + candidato1 + " votos Maria");
