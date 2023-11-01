557b4c1c7ced6d7d2316f6eafa7a47b1e7c5f602
juliana <jrtokura@gmail.com>
2023-11-01 11:00:53 -0300

    feat: Desafio#8

--- urnaEletronica.js
+++ urnaEletronica.js
@@ -1,9 +1,9 @@
-// function DataHora () {
-//     const dataHora = new Date ();
-//     console.log ("Data: "+ datahora.getdate() + (dataHora.getMonth +1) + dataHoraget. )
-// }
+function DataHora () {
+    const dataHora = new Date ();
+    return `Data: ${dataHora.getDate()}/${dataHora.getMonth() +1}/${dataHora.getFullYear()} ${dataHora.getHours()}:${dataHora.getMinutes()}:${dataHora.getSeconds()}`;
+}
 
-function verificarIntegridadeUrna() {
+ async function verificarIntegridadeUrna() {
 
     fetch('./urnaEletronica.js')
         .then (conteudo => conteudo.text())
@@ -24,87 +24,89 @@ function verificarIntegridadeUrna() {
 function urnaEletronica() {
 
     let
-        opcao;
+        voto;
         contadorTotal = 0,
         candidato1 = 0,
         candidato2 = 0,
         candidato3 = 0,
+        candidato4 = 0,
+        candidato5 = 0,
         branco = 0,
         nulo = 0;
         encerrar= "",
         pin=0;
         dataAtual = new Date();
-        // DadoC = [
-        //     [70,"Matilde"], 
-        //     [60,"Ester"], 
-        //     [50,"João"],
-        //     [40,"José"],
-        //     [30,"Maria"],
-        //     [10, "Branco"]
-        // ];
-        
-        // for (let i = 0; i < DadoC.length; i++) {
-        //     for (let j =0; j<DadoC.length; j++) {
-        //         console.log('Candidato: ' + DadoC[i][j]);
+        DadoC = [
+            [70,"Matilde", "AB"], 
+            [60,"Ester", "CD"], 
+            [50,"João", "EF"],
+            [40,"José", "GH"],
+            [30,"Maria", "IJ"],
+            [10, "Branco", "LM"]
+        ];
+      
 
-        //     }
-        // }
- 
-    do{
-            // Desafio#8
+        
+        for (let i = 0; i < DadoC.length; i++) {
+                console.log('Candidato: ' + DadoC[i][0] + " " + DadoC[i][1] + " " + DadoC[i][2]);
+        }
+        do {
+        
             pin=parseInt(prompt("Senha mesário: "));  
-            nomeCandidato1= prompt("Digite o nome do candidato 1: ");
-            nomeCandidato2= prompt("Digite o nome do candidato 2: ");
-            nomeCandidato3=prompt("Digite o nome do candidato 3: ");
-            console.log ("**NOMES DOS CANDIDATOS**");
-            console.log ('Candidato 1:  ' + nomeCandidato1);
-            console.log ('Candidato 2:  ' + nomeCandidato2);
-            console.log ('Candidato 3:  ' + nomeCandidato3);
+ 
 
         }while (!confirm("Se correto clicar  'OK' se incorreto 'Cancelar'"));
 
+        dataInicial = DataHora();
     do {
-        console.clear ();
-        opcao = parseInt(prompt("Inicio da votação" + '\n' +
-            "| 1 | " + nomeCandidato1+'\n' +
-            "| 2 | " + nomeCandidato2 + '\n' +
-            "| 3 | " + nomeCandidato3 + '\n' +
-            "| 5 | branco " + '\n' + '\n' +
-            "Digite a opção: "));
-    
-        contadorTotal++;
-    
 
-        if (opcao == 1) {
-            console.log("Você votou na Maria");
-            // #Desafio7
-            if (confirm("você votou no candidato 1, clique 'Ok' para confirmar ou 'cancel' para cancelar.")) {
+        voto = prompt ("Entre seu voto:");
+        contadorTotal++;
+        
+        if (voto == DadoC[0][0]) {
+            console.log(`Você votou:  ${DadoC[0][1]}`);
+            if (confirm(`você votou no ${DadoC[0][1]}, clique 'Ok' para confirmar ou 'cancel' para cancelar.`)) {
                 candidato1++;
             } else {
                 contadorTotal--;
             }
             
-        } else if (opcao == 2) {
-            console.log("Você votou no José");
-            if (confirm("você votou no candidato 2, clique 'Ok' para confirmar ou 'cancel' para cancelar.")){
+        } else if (voto == DadoC[1][0]) {
+            console.log(`Você votou:  ${DadoC[1][1]}`);
+            if (confirm("você votou no Ester, clique 'Ok' para confirmar ou 'cancel' para cancelar.")){
                 candidato2++;
             }else {
                 contadorTotal--;
             }
 
-        } else if (opcao == 3) {
-            console.log("Você votou no João");
-            if (confirm("você votou no candidato 3, clique 'Ok' para confirmar ou 'cancel' para cancelar.")) {
+        } else if (voto == DadoC[2][0]) {
+            console.log(`Você votou:  ${DadoC[2][1]}`);
+            if (confirm("você votou no João, clique 'Ok' para confirmar ou 'cancel' para cancelar.")) {
                 candidato3++;
             }else {
                 contadorTotal--;
             }
+        } else if (voto == DadoC[3][0]) {
+            console.log(`Você votou:  ${DadoC[3][1]}`);
+            if (confirm("você votou no José, clique 'Ok' para confirmar ou 'cancel' para cancelar.")) {
+                candidato4++;
+            }else {
+                contadorTotal--;
+            }
+        } else if (voto == DadoC[4][0]) {
+            console.log(`Você votou:  ${DadoC[4][1]}`);
+            if (confirm("você votou no Maria, clique 'Ok' para confirmar ou 'cancel' para cancelar.")) {
+                candidato5++;
+            }else {
+                contadorTotal--;
+            }
 
-        } else if (opcao == 5) {
-            console.log("Branco");
+        } else if (voto == DadoC[5][0]) {
+            console.log(`Você votou:  ${DadoC[5][1]}`);
+            
             branco++;
 
-        } else if (opcao == 123456) {
+        } else if (voto == 123456) {
             encerrar = prompt("Deseja encerrar a votação? Digite [S] para sim e [N] para não").toUpperCase();
                 if (encerrar !== 'S' && encerrar !== 'N') {
                     alert("Opção inválida");
@@ -124,23 +126,26 @@ function urnaEletronica() {
     } while (encerrar !== 'S')
 
     // console.clear();
-    dataFinal = new Date();
+    
     verificarIntegridadeUrna();
+    dataFinal = DataHora();
     console.log ("**Boletim de votação**");
-    console.log ("**Inicío da votação**" + " " + dataAtual);
-    console.log ('**Fim da Votação**' + " " + dataFinal);
+    console.log ("**Inicío da votação**" + " " + dataInicial);
+    console.log (`**Fim da Votação** ${dataFinal}`);
 
-    console.log ("Total de votos: " + contadorTotal);
-    console.log("Total de votos candidato 1: " + nomeCandidato1 + " votos (" + (candidato1 / contadorTotal * 100).toFixed(2) + " % )");
-    console.log("Total de votos candidato 2: " + nomeCandidato2 + " votos (" + (candidato2 / contadorTotal * 100).toFixed(2) + " % )");
-    console.log("Total de votos candidato 3: " + nomeCandidato3 + " votos (" + (candidato3 / contadorTotal * 100).toFixed(2) + " % )");
-    console.log("Total votos em branco: " + branco + " votos (" + (branco / contadorTotal * 100).toFixed(2) + " % )");
+    console.log (`Total de votos: ${contadorTotal}`);
+    console.log("Total de votos candidato 1: " + DadoC[0][1] + " votos (" + (DadoC[0][1] / contadorTotal * 100).toFixed(2) + " % )");
+    console.log("Total de votos candidato 2: " + DadoC[1][1] + " votos (" + (DadoC[1][1] / contadorTotal * 100).toFixed(2) + " % )");
+    console.log("Total de votos candidato 3: " + DadoC[2][1] + " votos (" + (DadoC[2][1]  / contadorTotal * 100).toFixed(2) + " % )");
+    console.log("Total de votos candidato 3: " + DadoC[3][1] + " votos (" + (DadoC[3][1]  / contadorTotal * 100).toFixed(2) + " % )");
+    console.log("Total de votos candidato 3: " + DadoC[4][1] + " votos (" + (DadoC[4][1]  / contadorTotal * 100).toFixed(2) + " % )");
+    console.log("Total votos em branco: " + DadoC[5][1] + " votos (" + (DadoC[5][1] / contadorTotal * 100).toFixed(2) + " % )");
     console.log("Total votos nulos: " + nulo + " votos (" + (nulo / contadorTotal * 100).toFixed(2) + " % )");
 
     
-    if (candidato1 > candidato2 && candidato1 > candidato3) {
-        candidato1=candidato1+branco;
-        console.log("Candidato 1 ganhador: " + candidato1 + " votos Maria");
+    if (DadoC[0][1] > DadoC[1][1] && DadoC[0][1] > DadoC [2][1]) {
+        DadoC[0][1]=DadoC[0][1]+[5][1];
+        console.log(`Candidato: &{DadoC[0][1]} ganhador`);
 
     } else if (candidato2 > candidato1 && candidato2 > candidato3) {
         candidato2=candidato2+branco;
